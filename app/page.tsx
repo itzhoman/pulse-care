16c6857520e6888bba5f3ceafd986654de8cffbb
unknown <psnforhoman@gmail.com>
2025-04-30 13:33:58 +0330

    shadcn button and dark theme added

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="flex h-screen max-h-screen text-white">
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default Home;
